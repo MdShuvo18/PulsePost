67ca5779d3412c975eb3f6b48d0e78048798580a
MdShuvo18 <mdks33881@gmail.com>
2024-05-10 16:23:53 +0600

    project setup done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
