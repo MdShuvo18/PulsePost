dd2404d495de1758932fed71301f3e4402997d27
MdShuvo18 <mdks33881@gmail.com>
2024-05-11 23:15:45 +0600

    category filter added to the AllBlogs section

--- src/Pages/AllBlogs.jsx
+++ src/Pages/AllBlogs.jsx
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Navbar from "./Home/Navbar";
 import { Card } from 'antd';
 import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 const { Meta } = Card;
 
@@ -12,19 +13,41 @@ const AllBlogs = () => {
     const allBlogs = useLoaderData()
     console.log(allBlogs)
     const navigate = useNavigate()
+    const [category, setCategoryFilter] = useState(null);
 
     const handlebutton = (id) => {
         navigate(`/blogdetails/${id}`)
+    }
+
+    const handleCustomization = (category) => {
+        const toLowerCase = category.toLowerCase();
+        setCategoryFilter(toLowerCase)
     }
     return (
         <div className="space-y-6">
             <Navbar></Navbar>
             <h2 className="text-5xl font-bold text-center">All blogs</h2>
+
+            <div className="grid justify-items-center">
+                <div className="dropdown dropdown-hover">
+                    <div tabIndex={0} role="button" className="btn m-1">Category</div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><button onClick={() => handleCustomization("Travel")}>Travel</button></li>
+                        <li><button onClick={() => handleCustomization("Food & Cooking")}>Food & Cooking</button></li>
+                        <li><button onClick={() => handleCustomization("Business & Entrepreneurship")}>Business & Entrepreneurship</button></li>
+                        <li><button onClick={() => handleCustomization("Health & Wellness")}>Health & Wellness</button></li>
+                        <li><button onClick={() => handleCustomization("Home & Garden")}>Home & Garden</button></li>
+                        <li><button onClick={() => handleCustomization("Photography")}>Photography</button></li>
+
+                    </ul>
+
+                </div>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center">
 
 
                 {
-                    allBlogs.map(allBlog => <Card className="" key={allBlog._id}
+                    allBlogs.filter(item => category === null || item.category.toLowerCase() === category).map(allBlog => <Card className="" key={allBlog._id}
                         hoverable
                         style={{ width: 240 }}
                         cover={<img alt="example" src={allBlog.image} />}
